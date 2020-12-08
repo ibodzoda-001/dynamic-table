9a1a9f854e07e9428445969ab3625be99b8b7fe0
fayzzzm <m.fayzov1@gmail.com>
2020-12-08 14:22:37 +0500

    Configured angular.json & launched ng-zorro component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'dynamic-table';
+  title = "dynamic-table";
+  expandSet = new Set<number>();
+
+  onExpandChange(id: number, checked: boolean): void {
+    if (checked) {
+      this.expandSet.add(id);
+    } else {
+      this.expandSet.delete(id);
+    }
+  }
+
+  listOfData = [
+    {
+      id: 1,
+      name: "John Brown",
+      age: 32,
+      expand: false,
+      address: "New York No. 1 Lake Park",
+      description:
+        "My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.",
+    },
+    {
+      id: 2,
+      name: "Jim Green",
+      age: 42,
+      expand: false,
+      address: "London No. 1 Lake Park",
+      description:
+        "My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.",
+    },
+    {
+      id: 3,
+      name: "Joe Black",
+      age: 32,
+      expand: false,
+      address: "Sidney No. 1 Lake Park",
+      description:
+        "My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.",
+    },
+  ];
 }
